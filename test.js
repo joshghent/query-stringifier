3d1a6ae5aa367b82bebf28722f5714dd996f3630
Josh Ghent <me@joshghent.com>
2016-10-07 19:56:57 +0100

    Added test for basic functionality of stringify method.

---
+++ test.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var qs = require('./');
+
+describe('query-stringifier', function () {
+    describe('#stringify', function () {
+        it('converts an object to a query string', function () {
+            var obj = {
+                food : 'pizza',
+                bar : 'chocolate'
+            };
+            var queryString = qs.stringify(obj);
+
+            expect(queryString).to.equal('food=pizza&bar=chocolate');
+        });
+    });
+});
