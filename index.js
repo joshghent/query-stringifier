68b5eae19b5fd57079be7d72a84d013992b0a660
Josh Ghent <me@joshghent.com>
2016-10-07 19:45:11 +0100

    Started stringify method

---
+++ index.js
@@ -0,0 +1,12 @@
+// Create our constructor
+const QueryString = function () {};
+
+// Add the query stringify method
+// Arguments:
+//  @params - OBJECT - An object of parameters to be built into a query string
+QueryString.prototype.stringify = function (params) {
+
+};
+
+// Export the module
+module.exports = new QueryString();
